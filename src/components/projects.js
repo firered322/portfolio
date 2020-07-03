30f950c3461f8c7ad688196132dba3978c138d7b
Atul <atulsingh1396@gmail.com>
2020-07-03 13:58:32 +0530

    added projects section with individual project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,58 @@
 import React from "react";
+import { Card, CardTitle, Button, CardActions, CardText } from "react-mdl";
 
-const Projects = () => <h1>Projects Page</h1>;
+const Projects = () => {
+  return (
+    <div className="container">
+      <div className="projects-grid">
+        <div className="project-item">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle style={{ color: "#000", height: "176px" }}>
+              React{" "}
+            </CardTitle>
+            <CardText>
+              Hello this is a sample react project description. This is dummy
+              text and actually does not have any meaning of its own.
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+          </Card>
+        </div>
+        <div className="project-item">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle style={{ color: "#000", height: "176px" }}>
+              React{" "}
+            </CardTitle>
+            <CardText>
+              Hello this is a sample react project description. This is dummy
+              text and actually does not have any meaning of its own.
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+          </Card>
+        </div>
+        <div className="project-item">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle style={{ color: "#000", height: "176px" }}>
+              React{" "}
+            </CardTitle>
+            <CardText>
+              Hello this is a sample react project description. This is dummy
+              text and actually does not have any meaning of its own.
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
