d22c40973498501150088f5e54f283f2f5a4e29d
Atul <atulsingh1396@gmail.com>
2020-07-03 23:44:32 +0530

    Updates

---
+++ precache-manifest.fbd2c1043136d5b62b8d7c9bf331ff30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ffdd07994733a0f18670c538175141",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e0e611f76a9b296fd61",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "5036e76fabfc2fc6131b",
+    "url": "/portfolio/static/css/main.3a8fc8d4.chunk.css"
+  },
+  {
+    "revision": "8e0e611f76a9b296fd61",
+    "url": "/portfolio/static/js/2.71b647b3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.71b647b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5036e76fabfc2fc6131b",
+    "url": "/portfolio/static/js/main.b16f59c7.chunk.js"
+  },
+  {
+    "revision": "039eb055909a0ac81ca5",
+    "url": "/portfolio/static/js/runtime-main.3e7fea94.js"
+  }
+]);
